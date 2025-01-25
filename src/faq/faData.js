142c86218e0893b9aa85997bfb4b0420cf24d5b6
magmadvlab <35226689+magmadvlab@users.noreply.github.com>
2025-01-25 14:22:48 +0100

    Create faData.js

---
+++ src/faq/faData.js
@@ -0,0 +1,29 @@
+// Import delle FAQ individuali
+import activitiesAndAttractions_IT from './it/activitiesAndAttractions_IT';
+import checkin_IT from './it/checkin_it';
+import checkout_IT from './it/checkout_it';
+import dining_IT from './it/dining_it';
+import emergency_IT from './it/emergency_it';
+import entertainment_IT from './it/entertainment_it';
+import pets_IT from './it/pets_it';
+import ski_IT from './it/ski_it';
+import techServices_IT from './it/tech_services_it';
+import transport_IT from './it/transport_it';
+import wellness_IT from './it/wellness_it';
+
+// Organizzazione delle FAQ per categoria
+const faqData = {
+  activitiesAndAttractions_IT,
+  checkin_IT,
+  checkout_IT,
+  dining_IT,
+  emergency_IT,
+  entertainment_IT,
+  pets_IT,
+  ski_IT,
+  techServices_IT,
+  transport_IT,
+  wellness_IT,
+};
+
+export default faqData;
